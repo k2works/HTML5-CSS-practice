07c7146f2d308f173e9ca1a40a48bf528a2ca280
k2works <kakimomokuri@gmail.com>
2016-10-05 07:57:22 +0900

    Update 2016-10-04T22:57:18.993Z

---
+++ web.js
@@ -0,0 +1,13 @@
+/**
+ * Created by k2works on 2016/10/05.
+ */
+
+var express = require('express');
+var http = require('http');
+var gzippo = require('gzippo');
+var logger = require('morgan');
+var app = express();
+app.use(logger());
+app.use(gzippo.staticGzip('' + __dirname));
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
